321f0f159b0f07a79d1674b0b7403525bd0bcb09
phulelouch <62769629+phulelouch@users.noreply.github.com>
2022-06-07 21:51:02 +1000

    comment

--- Project/server/index.php
+++ Project/server/index.php
@@ -1,28 +1,28 @@
 <?php
-if (isset($_POST['gas'])) {
+if (isset($_POST['gas'])) { //read gas value with post request and put it in a txt file
 	        echo $_POST['gas'];
 		        $myfile = fopen("gas.txt", "w");
 		        fwrite($myfile, $_POST['gas']);
 			        fclose($myfile);
-} else if (isset($_POST['flame'])) {
+} else if (isset($_POST['flame'])) {//read flame value with post request and put it in a txt file
 	        echo $_POST['flame'];
 		        $myfile = fopen("flame.txt", "w");
 		        fwrite($myfile, $_POST['flame']);
 			        fclose($myfile);
-} else if (isset($_POST['button'])) {
+} else if (isset($_POST['button'])) { //read button status with post request and put it in a txt file, 1 mean button is pressed
 	        echo $_POST['button'];
 		        $myfile = fopen("button.txt", "w");
 		        fwrite($myfile, $_POST['button']);
 			        fclose($myfile);
-} else if(isset($_GET['read'])&&($_GET['read']==1)){
+} else if(isset($_GET['read'])&&($_GET['read']==1)){ //read gas value
 	        $myfile = fopen("gas.txt", "r");
 		        echo fread($myfile,filesize("gas.txt"));
 		        fclose($myfile);
-} else if(isset($_GET['read'])&&($_GET['read']==2)){
+} else if(isset($_GET['read'])&&($_GET['read']==2)){//read flame value
 	        $myfile = fopen("flame.txt", "r");
 		        echo fread($myfile,filesize("flame.txt"));
 		        fclose($myfile);
-} else if(isset($_GET['read'])&&($_GET['read']==3)){
+} else if(isset($_GET['read'])&&($_GET['read']==3)){//read button state value
 	        $myfile = fopen("button.txt", "r");
 		        echo fread($myfile,filesize("button.txt"));
 		        fclose($myfile);
